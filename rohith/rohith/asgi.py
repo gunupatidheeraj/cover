42d8f96ba0607745704676efbec063b093fc838a
rohithvijay01 <rohithvijay.gvr@gmail.com>
2023-12-01 12:24:55 +0530

    Success

---
+++ rohith/rohith/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rohith project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rohith.settings')
+
+application = get_asgi_application()
